43a3a66df8ff13a8d4ca36e0b1efaa1124a7bc14
Dimitris Fasoulas <dimitris@globalmouth.com>
2016-02-22 15:13:34 +0100

    Added statistics API call New build

--- src/js/controllers/appController.js
+++ src/js/controllers/appController.js
@@ -1,65 +1,83 @@
 (function () {
 
-   'use strict';
+      'use strict';
 
-   function appController( $scope, $http, $q, $widgetService, $apiService, $controller ) {
+      function appController( $scope, $http, $q, $widgetService, $apiService, $controller, $statisticsApi ) {
 
-      // Extend the core controller that takes care of basic setup and common functions
-      angular.extend(appController, $controller('widgetCoreController', {
-         '$scope': $scope
-      }));
+         // Extend the core controller that takes care of basic setup and common functions
+         angular.extend(appController, $controller('widgetCoreController', {
+            '$scope': $scope
+         }));
 
-      $scope.leagueTableRows = [];
+         $scope.leagueTableRows = [];
 
-      $scope.columnLabels = {};
+         $scope.columnLabels = {};
 
-      $scope.defaultHeight = 450;
+         $scope.defaultHeight = 450;
 
-      $scope.calculatedHeight = 0;
+         $scope.calculatedHeight = 0;
 
-      $scope.rowHeight = 45;
+         $scope.rowHeight = 45;
 
-      /**
-       * Get league items
-       */
-      $scope.getLeagueItems = function () {
+         //Get page info
+         $widgetService.requestPageInfo();
 
-         // Temporary call to local file to get the league data.
-         $http({
-            method: 'GET',
-            url: 'ofe_output_v1.json'
-         }).then(function successCallback( objResponse ) {
-            if ( objResponse.data && objResponse.data.statistics ) {
-               var i = 0, arrLength = objResponse.data.statistics.length;
-               for ( ; i < arrLength; ++i ) {
-                  var statistics = objResponse.data.statistics[i];
-
-                  if ( statistics && statistics.leagueTable ) {
-                     $scope.columnLabels = statistics.leagueTable.columnLabels;
-                     $scope.leagueTableRows = statistics.leagueTable.leagueTableRows;
-                     $scope.calculatedHeight = $scope.leagueTableRows.length * $scope.rowHeight;
-                  }
+         /**
+          * Get column labels from local file
+          */
+         $scope.getColumnLabels = function () {
+            $http({
+               method: 'GET',
+               url: 'labels.json'
+            }).then(function successCallback( objResponse ) {
+               if ( objResponse.data && objResponse.data.columnLabels ) {
+                  $scope.columnLabels = objResponse.data.columnLabels
                }
+            });
+         }
 
-               $scope.setWidgetHeight($scope.calculatedHeight);
-            }
+         /**
+          * Get league items
+          */
+         $scope.getLeagueItems = function ( type, filter ) {
 
-         }, function errorCallback( response ) {
+            $scope.getColumnLabels();
+            $statisticsApi.getStatistics(type, filter)
+               .then(function successCallback( objResponse ) {
+                  if ( objResponse.data && objResponse.data.statistics ) {
+                     var i = 0, arrLength = objResponse.data.statistics.length;
+                     for ( ; i < arrLength; ++i ) {
+                        var statistics = objResponse.data.statistics[i];
+
+                        if ( statistics && statistics.leagueTable ) {
+                           $scope.leagueTableRows = statistics.leagueTable.leagueTableRows;
+                           $scope.calculatedHeight = $scope.leagueTableRows.length * $scope.rowHeight;
+                        }
+                     }
+
+                     $scope.setWidgetHeight($scope.calculatedHeight);
+                  }
+
+               }, function errorCallback( response ) {
+               });
+         };
+
+         // Call the init method in the coreWidgetController so that we setup everything using our overridden values
+         // The init-method returns a promise that resolves when all of the configurations are set, for instance the $scope.args variables
+         // so we can call our methods that require parameters from the widget settings after the init method is called
+         $scope.init().then(function () {
+            // Fetch League statistics
+            $scope.filter = $scope.pageInfo.pageParam;
+            $scope.getLeagueItems('leaguetable', $scope.filter);
          });
-      };
 
-      // Call the init method in the coreWidgetController so that we setup everything using our overridden values
-      // The init-method returns a promise that resolves when all of the configurations are set, for instance the $scope.args variables
-      // so we can call our methods that require parameters from the widget settings after the init method is called
-      $scope.init().then(function () {
-         // Fetch the live events
-         $scope.getLeagueItems();
-      });
+      }
+
+      (function ( $app ) {
+         return $app.controller('appController', ['$scope', '$http', '$q', 'kambiWidgetService', 'kambiAPIService', '$controller', 'kambiStatisticsService',
+            appController]);
+      })(angular.module('leagueWidget'));
 
    }
 
-   (function ( $app ) {
-      return $app.controller('appController', ['$scope', '$http', '$q', 'kambiWidgetService', 'kambiAPIService', '$controller', appController]);
-   })(angular.module('leagueWidget'));
-
-}).call(this);
+).call(this);
