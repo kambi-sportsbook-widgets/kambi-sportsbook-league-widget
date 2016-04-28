a831add4012fc5f3e0d896bd1ede7f1da7a657e3
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-04-28 15:13:16 +0200

    Updated to new version of core-library and build-tools

--- src/js/app.js
+++ src/js/app.js
@@ -1,59 +1,49 @@
 (function () {
    'use strict';
 
-   var LeagueTable = Stapes.subclass({
-      constructor: function () {
-         this.scope = {
-            args: {
-               updatedTime: '',
-               headerDefaultTitle: ''
-            },
-            leagueTableRows: []
-         };
+   var LeagueTable = CoreLibrary.Component.subclass({
+      defaultArgs: {
+         updatedTime: '',
+         headerDefaultTitle: ''
+      },
+
+      constructor () {
+         CoreLibrary.Component.apply(this, arguments);
+      },
+
+      init () {
+         this.scope.leagueTableRows = [];
+
          this.getColumnLabels();
 
-         CoreLibrary.init()
-            .then(function ( widgetArgs ) {
-               Object.keys(widgetArgs).forEach(function ( key ) {
-                  this.scope.args[key] = widgetArgs[key];
-               }.bind(this));
+         CoreLibrary.widgetModule.enableWidgetTransition(true);
 
-               CoreLibrary.widgetModule.enableWidgetTransition(true);
+         CoreLibrary.widgetModule.setWidgetHeight(450);
 
-               CoreLibrary.widgetModule.setWidgetHeight(450);
+         // for testing:
+         // CoreLibrary.pageInfo.leaguePaths = ['football/england/premier_league/'];
 
-               // for testing:
-               // CoreLibrary.pageInfo.leaguePaths = ['football/england/premier_league/'];
+         if ( CoreLibrary.pageInfo.leaguePaths != null && CoreLibrary.pageInfo.leaguePaths.length === 1 ) {
+            CoreLibrary.statisticsModule
+               .getStatistics('leaguetable', CoreLibrary.pageInfo.leaguePaths[0])
+               .then(( data ) => {
+                  var rows = [], date = new Date(data.updated);
+                  data.leagueTableRows.forEach(( row ) => {
+                     row.goalsDifference = row.goalsFor - row.goalsAgainst;
+                     rows.push(row);
+                  });
+                  this.scope.leagueTableRows = rows;
+                  this.scope.args.updatedTime = date;
 
-               if ( CoreLibrary.pageInfo.leaguePaths != null && CoreLibrary.pageInfo.leaguePaths.length === 1 ) {
-                  CoreLibrary.statisticsModule
-                     .getStatistics('leaguetable', CoreLibrary.pageInfo.leaguePaths[0])
-                     .then(function ( data ) {
-                        var rows = [], date = new Date(data.updated);
-                        data.leagueTableRows.forEach(function ( row ) {
-                           row.goalsDifference = row.goalsFor - row.goalsAgainst;
-                           rows.push(row);
-                        }.bind(this));
-                        this.scope.leagueTableRows = rows;
-                        this.scope.args.updatedTime = date;
+                  // Calculate the height based on the rows plus the header and footer divs
+                  var rowHeight = 45;
+                  var calculatedHeight = this.scope.leagueTableRows.length * rowHeight + 84;
 
-                        // Calculate the height based on the rows plus the header and footer divs
-                        var rowHeight = 45;
-                        var calculatedHeight = this.scope.leagueTableRows.length * rowHeight + 84;
-
-                        CoreLibrary.widgetModule.setWidgetHeight(calculatedHeight);
-                     }.bind(this));
-               } else {
-                  CoreLibrary.widgetModule.removeWidget();
-               }
-
-            }.bind(this))
-            .catch(function ( error ) {
-               console.debug('init error');
-               console.trace(error);
-            });
-
-         this.view = rivets.bind(document.getElementById('main'), this.scope);
+                  CoreLibrary.widgetModule.setWidgetHeight(calculatedHeight);
+               });
+         } else {
+            CoreLibrary.widgetModule.removeWidget();
+         }
 
          this.view.binders['column-header-title'] = function ( el, column ) {
             if ( column.value !== 'Pos' && column.value !== 'participantName' ) {
@@ -65,10 +55,10 @@
             if ( tableRow != null ) {
                return tableRow[column.key];
             }
-         }.bind(this);
+         };
       },
 
-      getColumnLabels: function () {
+      getColumnLabels () {
          this.scope.columnLabels = [
             {
                key: 'position',
@@ -105,6 +95,8 @@
       }
    });
 
-   var leagueTable = new LeagueTable();
+   var leagueTable = new LeagueTable({
+      rootElement: 'html'
+   });
 
 })();
