5e1202cc6ddf9754c3428d4f7f6a541d0350ec08
Karol Maciaszek <karol@globalmouth.com>
2017-02-28 16:12:01 +0100

    Fixed rows displaying

--- src/js/Components/TableBody/TableBody.jsx
+++ src/js/Components/TableBody/TableBody.jsx
@@ -2,9 +2,14 @@ import React, { Children, PropTypes } from 'react';
 import styles from './TableBody.scss';
 
 const TableBody = ({ children }) => {
+   // todo: use TableBodyRow (KSBWI-908)
+   if (!children instanceof Array) {
+      children = [];
+   }
+
    return (
       <tbody>
-         {Children.map(children, (row, i) =>
+         {children.map((row, i) =>
             <tr key={i} className={['KambiWidget-card-inner-border', styles.row].join(' ')}>
                {row}
                <td className='margin' />
